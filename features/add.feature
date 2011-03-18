f98f344f43e63a64a8c9456d8ee7d67a858ff07e
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2011-03-18 19:39:47 +0000

    changed cucumber example

--- features/add.feature
+++ features/add.feature
@@ -1,11 +1,9 @@
-Feature: Create
-  In order to avoid silly mistakes
-  As a sailor
-  I want to be told the details of the boat
+Feature: Create a boat
+  In order to avoid mistakes when finding my boat
+  As a sailor of my boat
+  I want to be told the details of my boat
 
-  Scenario: Create a new boat
-    Given I create a boat
-    And I fill in 'Ruby' for 'name'
-    And I fill in 'red' for 'colour'
-    When I check the boat details
-    Then I should see 'Ruby'
+  Scenario: Creating a new boat
+    Given a  new boat of the name 'ruby' and the colour 'red'
+    When I ask for the details of the boat
+    Then I should see 'Ruby' as the name, and 'red' as the colour
--- features/step_definitions/add_steps.rb
+++ features/step_definitions/add_steps.rb
@@ -1,24 +1,12 @@
-require 'boat'
-require 'test/unit/assertions'
-World(Test::Unit::Assertions)
-
-Given /^I create a boat$/ do
-  boat = Boat.new
-  assert_equal("Sunrise", boat.name)
+Given /^a  new boat of the name 'ruby' and the colour 'red'$/ do
+ @boat = Boat.new(name='ruby', colour='red')  
+ 
 end
 
-Given /^I fill in 'Ruby' for 'name'$/ do
+When /^I ask for the details of the boat$/ do
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^I fill in 'red' for 'colour'$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-When /^I check the boat details$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should see 'Ruby'$/ do
+Then /^I should see 'Ruby' as the name, and 'red' as the colour$/ do
   pending # express the regexp above with the code you wish you had
 end
